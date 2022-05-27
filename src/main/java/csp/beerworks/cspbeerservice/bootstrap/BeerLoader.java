57e49dc52205136c372fbe955f411cbd96ab09a4
saipraveen99 <chsaipraveen33@gmail.com>
2022-05-27 17:33:05 +0000

    Loading beer data resolved

--- src/main/java/csp/beerworks/cspbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/csp/beerworks/cspbeerservice/bootstrap/BeerLoader.java
@@ -7,9 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
-//@Component
-//@Slf4j
+@Component
+@Slf4j
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -29,33 +30,36 @@ public class BeerLoader implements CommandLineRunner {
     private void loadBeerObjects() {
         if (this.beerRepository.count() == 0) {
             beerRepository.save(Beer.builder()
+                            //.id(UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb"))
                             .beerName("Mango Bobs")
                             .beerStyle("IPA")
                             .upc(BEER_1_UPC)
                             .quantityToBrew(200)
                             .minOnHand(12)
-                            .price(new BigDecimal(200))
+                            .price(new BigDecimal(12.95))
                     .build());
 
             beerRepository.save(Beer.builder()
+                    //.id(UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd"))
                     .beerName("Galaxy Cat")
                     .beerStyle("PALE_ALE")
                     .upc(BEER_2_UPC)
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .price(new BigDecimal(240))
+                    .price(new BigDecimal(12.95))
                     .build());
 
             beerRepository.save(Beer.builder()
-                    .beerName("BIRA")
-                    .beerStyle("PUNCH")
+                    //.id(UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08"))
+                    .beerName("Pinball Porter")
+                    .beerStyle("PORTER")
                     .upc(BEER_3_UPC)
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .price(new BigDecimal(260))
+                    .price(new BigDecimal(12.95))
                     .build());
 
-            //log.info("Loaded beers: "+beerRepository.count());
+            log.debug("Loaded beers: "+beerRepository.count());
         }
     }
 }
